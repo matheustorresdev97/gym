5342b028bde67b14eb96eaa1acc03f5d08cae57a
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-13 20:37:46 -0300

    feat: add input component

--- src/app/(auth)/sign-in.tsx
+++ src/app/(auth)/sign-in.tsx
@@ -1,8 +1,11 @@
 import { Center } from "@/components/ui/center";
 import { VStack } from "@/components/ui/vstack";
+import { Heading } from "@/components/ui/heading";
 import { Image, Text } from "react-native";
 
-import Logo from '@/assets/logo.svg'
+import Logo from '@/assets/logo.svg';
+
+import { Input } from "@/components/input";
 
 export default function SignIn() {
     return (
@@ -14,13 +17,20 @@ export default function SignIn() {
                 alt="Pessoas treinando"
             />
 
-            <Center className="my-24">
-                <Logo />
+            <VStack className="flex-1 px-10 pb-16">
+                <Center className="my-24">
+                    <Logo />
+                    <Text className="text-gray100 text-sm">
+                        Treine sua mente e seu corpo
+                    </Text>
+                </Center>
 
-                <Text className="text-gray100 text-sm">
-                    Treine sua mente e seu corpo
-                </Text>
-            </Center>
+                <Center className="gap-2">
+                    <Heading className="text-gray100">Acesse sua conta</Heading>
+                    <Input placeholder="E-mail" />
+                    <Input placeholder="Senha" />
+                </Center>
+            </VStack>
         </VStack>
     )
 }
---
+++ src/components/input.tsx
@@ -0,0 +1,22 @@
+import type { ComponentProps } from 'react'
+import { Input as GluestackInput, InputField } from '@/components/ui/input'
+
+import colors from 'tailwindcss/colors'
+
+
+type InputProps = ComponentProps<typeof InputField>
+
+export function Input({ ...props }: InputProps) {
+    return (
+        <GluestackInput
+            className="bg-gray700 h-14 px-4 border border-transparent rounded-md data-[focus=true]:border-green500"
+            focusable
+        >
+            <InputField
+                className="text-white font-regular"
+                placeholderTextColor={colors.gray[300]}
+                {...props}
+            />
+        </GluestackInput>
+    )
+}
