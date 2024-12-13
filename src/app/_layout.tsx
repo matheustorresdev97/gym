733ed1d225842fdec86b40edd04cb6994b735c22
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-13 19:41:59 -0300

    feat: personalizando a statusbar

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,17 +1,23 @@
 import { Stack } from "expo-router"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
-
+import { StatusBar } from "react-native"
 import {
     useFonts,
     Roboto_700Bold,
     Roboto_400Regular,
 } from '@expo-google-fonts/roboto'
 
+
 export default function RootLayout() {
     const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular })
 
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
+            <StatusBar
+                barStyle="light-content"
+                backgroundColor="transparent"
+                translucent
+            />
             <Stack
                 screenOptions={{
                     headerShown: false,
