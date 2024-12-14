cd711274d94a63fa9b35f4c3aeea06c08f713777
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-14 20:23:10 -0300

    feat: add foto do user

--- src/app/(root)/profile.tsx
+++ src/app/(root)/profile.tsx
@@ -1,10 +1,27 @@
-import { Center } from "@/components/ui/center";
-import { Text } from "react-native";
+import { ScrollView } from 'react-native'
+
+import { VStack } from "@/components/ui/vstack";
+import { Center } from '@/components/ui/center';
+
+import { ScreenHeader } from "@/components/screen-header";
+import { UserPhoto } from '@/components/user-photo';
+
 
 export default function Profile() {
     return (
-        <Center className="flex-1 bg-colors-gray700">
-            <Text>Profile</Text>
-        </Center>
+        <VStack className="flex-1 bg-colors-gray700">
+            <ScreenHeader title="Perfil" />
+
+            <ScrollView contentContainerStyle={{ paddingBottom: 36 }}>
+                <Center className='mt-6 px-10'>
+                    <UserPhoto
+                        source={{ uri: 'https://github.com/matheustorresdev97.png' }}
+                        alt="Imagem do usuário"
+                       height={64}
+                       width={64}
+                    />
+                </Center>
+            </ScrollView>
+        </VStack>
     )
 }
