e93a38b7f8c3930786213a51e9169ce502762829
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-14 20:53:30 -0300

    feat: finalizando interface de exercicios

--- src/app/(root)/exercise.tsx
+++ src/app/(root)/exercise.tsx
@@ -1,12 +1,20 @@
+import { Image, Text, TouchableOpacity } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import { router } from "expo-router";
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { colors } from "@/styles/colors";
+
 import { Heading } from "@/components/ui/heading";
 import { HStack } from "@/components/ui/hstack";
 import { VStack } from "@/components/ui/vstack";
-import { colors } from "@/styles/colors";
-import AntDesign from '@expo/vector-icons/AntDesign';
-import { router } from "expo-router";
-import { Text, TouchableOpacity } from "react-native";
+import { Box } from "@/components/ui/box";
 
 import BodySvg from '@/assets/body.svg'
+import SeriesSvg from '@/assets/series.svg'
+import RepetitionsSvg from '@/assets/repetitions.svg'
+
+
+import { Button } from "@/components/button";
 
 export default function Exercise() {
 
@@ -37,6 +45,41 @@ export default function Exercise() {
                     </HStack>
                 </HStack>
             </VStack>
+
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={{ paddingBottom: 32 }}
+            >
+                <VStack className="p-8">
+                    <Image
+                        source={{
+                            uri: 'https://static.wixstatic.com/media/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp/v1/fill/w_350,h_375,al_c/2edbed_60c206e178ad4eb3801f4f47fc6523df~mv2.webp',
+                        }}
+                        alt="Exercício"
+                        className="mb-3 rounded-lg w-full h-4/5"
+                        resizeMode="stretch"
+                    />
+                    <Box className="bg-colors-gray600 rounded-md pb-4 px-4">
+                        <HStack
+                            className="items-center justify-around mb-6 mt-5"
+                        >
+                            <HStack>
+                                <SeriesSvg />
+                                <Text className="text-colors-gray200 ml-2">
+                                    3 séries
+                                </Text>
+                            </HStack>
+                            <HStack>
+                                <RepetitionsSvg />
+                                <Text className="text-colors-gray200 ml-2">
+                                    12 repetições
+                                </Text>
+                            </HStack>
+                        </HStack>
+                        <Button title="Marcar como realizado" />
+                    </Box>
+                </VStack>
+            </ScrollView>
         </VStack>
     )
 }
