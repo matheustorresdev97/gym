ee45ee747b4266ea81b32485404229bdbc31f766
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-14 14:33:03 -0300

    feat: alterando icons dos tabs screens

--- src/app/(root)/_layout.tsx
+++ src/app/(root)/_layout.tsx
@@ -1,11 +1,34 @@
 import { Tabs } from "expo-router";
 
+import HomeSvg from '@/assets/home.svg'
+import HistorySvg from '@/assets/history.svg'
+import ProfileSvg from '@/assets/profile.svg'
+
 export default function RootLayout() {
+
     return (
-        <Tabs screenOptions={{ headerShown: false }}>
-            <Tabs.Screen name="home" />
-            <Tabs.Screen name="history" />
-            <Tabs.Screen name="profile" />
+        <Tabs screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+            <Tabs.Screen name="home"
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <HomeSvg fill={color} width={18} height={18} />
+                    ),
+                }}
+            />
+            <Tabs.Screen name="history"
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <HistorySvg fill={color} width={18} height={18} />
+                    ),
+                }}
+            />
+            <Tabs.Screen name="profile"
+                options={{
+                    tabBarIcon: ({ color }) => (
+                        <ProfileSvg fill={color} width={18} height={18} />
+                    ),
+                }}
+            />
             <Tabs.Screen name="exercise" />
         </Tabs>
 
