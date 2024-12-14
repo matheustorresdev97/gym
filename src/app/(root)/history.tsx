acfa8bd25745792e2ad97fa060b30c953ebae308
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-14 20:18:03 -0300

    feat: history-card component

--- src/app/(root)/history.tsx
+++ src/app/(root)/history.tsx
@@ -1,12 +1,49 @@
+import { useState } from 'react'
+import { SectionList, Text } from 'react-native'
+
 import { VStack } from "@/components/ui/vstack";
 
 import { ScreenHeader } from "@/components/screen-header";
+import { HistoryCard } from "@/components/history-card";
+import { Heading } from '@/components/ui/heading';
 
 
 export default function History() {
+    const [exercises, setExercises] = useState([
+        {
+            title: '22.07.24',
+            data: ['Puxada frontal', 'Remada unilateral'],
+        },
+        {
+            title: '23.07.24',
+            data: ['Puxada frontal'],
+        },
+    ])
+
     return (
         <VStack className="flex-1 bg-colors-gray700">
             <ScreenHeader title="Histórico" />
+            <SectionList
+                sections={exercises}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => <HistoryCard />}
+                renderSectionHeader={({ section }) => (
+                    <Heading className='text-colors-gray200 text-base mt-10 mb-3'>
+                        {section.title}
+                    </Heading>
+                )}
+                style={{ paddingHorizontal: 32 }}
+                contentContainerStyle={
+                    exercises.length === 0 && { flex: 1, justifyContent: 'center' }
+                }
+                ListEmptyComponent={() => (
+                    <Text className='text-colors-gray200 text-center'>
+                        Não há exercícios registrados ainda. {'\n'}
+                        Vamos fazer execícios hoje?
+                    </Text>
+                )}
+                showsVerticalScrollIndicator={false}
+            />
         </VStack>
     )
 }
---
+++ src/components/history-card.tsx
@@ -0,0 +1,26 @@
+import { Text } from "react-native";
+import { Heading } from "./ui/heading";
+import { HStack } from "./ui/hstack";
+import { VStack } from "./ui/vstack";
+
+export function HistoryCard() {
+    return (
+        <HStack
+            className="w-full px-5 py-4 mb-3 bg-colors-gray600 rounded-md items-center justify-between"
+        >
+            <VStack className="mr-5">
+                <Heading
+                    className="text-white text-base font-heading capitalize"
+                >
+                    Costas
+                </Heading>
+                <Text className="text-colors-gray100 text-lg" numberOfLines={1}>
+                    Puxada frontal
+                </Text>
+            </VStack>
+            <Text className="text-colors-gray300 text-base">
+                08:56
+            </Text>
+        </HStack>
+    )
+}
