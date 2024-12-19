4ff9cebc280829899aa7f7bd0087d8f0624ca4f9
matheustorresdev97 <matheustorresdev@gmail.com>
2024-12-19 13:42:32 -0300

    feat: obtendo dados do user

--- src/app/(root)/profile.tsx
+++ src/app/(root)/profile.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
-import { Alert, ScrollView, Text, TouchableOpacity } from 'react-native'
+import { ScrollView, Text, TouchableOpacity } from 'react-native'
+import { Controller, useForm } from 'react-hook-form';
 
 import { VStack } from "@/components/ui/vstack";
 import { Center } from '@/components/ui/center';
@@ -14,7 +15,15 @@ import { useToast } from '@/components/ui/toast'
 import * as FileSystem from 'expo-file-system'
 import * as ImagePicker from 'expo-image-picker'
 import { ToastMessage } from '@/components/toast-message';
-;
+import { useAuth } from '@/contexts/AuthContext';
+
+type FormDataProps = {
+    name: string;
+    email: string;
+    password: string;
+    old_password: string;
+    confirm_password: string;
+}
 
 
 export default function Profile() {
@@ -23,6 +32,13 @@ export default function Profile() {
     )
 
     const toast = useToast()
+    const { user } = useAuth();
+    const { control, handleSubmit } = useForm<FormDataProps>({
+        defaultValues: {
+            name: user.name,
+            email: user.email
+        }
+    });
 
     async function handleUserPhotoSelect() {
         try {
@@ -64,6 +80,10 @@ export default function Profile() {
         }
     }
 
+    async function handleProfileUpdate(data: FormDataProps) {
+        console.log(data);
+    }
+
     return (
         <VStack className="flex-1 bg-colors-gray700">
             <ScreenHeader title="Perfil" />
@@ -85,8 +105,30 @@ export default function Profile() {
                     </TouchableOpacity>
 
                     <Center className='w-full gap-4'>
-                        <Input placeholder="Nome" className='bg-colors-gray600' />
-                        <Input value="matheus@email.com" className='bg-colors-gray600' isReadOnly />
+                        <Controller
+                            control={control}
+                            name="name"
+                            render={({ field: { value, onChange } }) => (
+                                <Input
+                                    className='bg-colors-gray600'
+                                    placeholder='Nome'
+                                    onChangeText={onChange}
+                                    value={value}
+                                />
+                            )} />
+
+                        <Controller
+                            control={control}
+                            name="email"
+                            render={({ field: { value, onChange } }) => (
+                                <Input
+                                    className='bg-colors-gray600'
+                                    placeholder="E-mail"
+                                    isReadOnly
+                                    onChangeText={onChange}
+                                    value={value}
+                                />
+                            )} />
                     </Center>
 
                     <Heading
@@ -95,14 +137,49 @@ export default function Profile() {
                         Alterar senha
                     </Heading>
                     <Center className='w-full gap-4'>
-                        <Input placeholder="Senha antiga" className='bg-colors-gray600' secureTextEntry />
-                        <Input placeholder="Nova senha" className='bg-colors-gray600' secureTextEntry />
-                        <Input
-                            placeholder="Confirme a nova senha"
-                            className='bg-colors-gray600'
-                            secureTextEntry
+                        <Controller
+                            control={control}
+                            name="old_password"
+                            render={({ field: { onChange } }) => (
+                                <Input
+                                    className='bg-colors-gray600'
+                                    placeholder="Senha antiga"
+                                    secureTextEntry
+                                    onChangeText={onChange}
+                                />
+                            )}
                         />
-                        <Button title="Atualizar" />
+
+                        <Controller
+                            control={control}
+                            name="password"
+                            render={({ field: { onChange } }) => (
+                                <Input
+                                    className='bg-colors-gray600'
+                                    placeholder="Nova senha"
+                                    secureTextEntry
+                                    onChangeText={onChange}
+                                />
+                            )}
+                        />
+                        <Controller
+                            control={control}
+                            name="confirm_password"
+                            render={({ field: { onChange } }) => (
+                                <Input
+                                    className='bg-colors-gray600'
+                                    placeholder="Confirme a nova senha"
+                                    secureTextEntry
+                                    onChangeText={onChange}
+                                />
+                            )}
+                        />
+
+                        <Button
+                            title="Atualizar"
+                            className='mt-4'
+                            onPress={handleSubmit(handleProfileUpdate)}
+                        />
                     </Center>
                 </Center>
             </ScrollView>
